54d841923faaf57c6f3d245d3553cd2c09d3d909
Sidharth Babu <95430324+sidhuiwnl@users.noreply.github.com>
2023-12-20 18:16:34 +0530

    mongodb connect

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('your link');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
