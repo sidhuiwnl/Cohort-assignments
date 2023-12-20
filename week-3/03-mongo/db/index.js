79f90c24fda661fa051051745354e8962ba346c4
Sidharth Babu <95430324+sidhuiwnl@users.noreply.github.com>
2023-12-20 22:49:17 +0530

    Added Schema

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,25 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your link');
+mongoose.connect('mongodb+srv://sidharthinfernal:Seemasidhu999@cluster0.sv727p9.mongodb.net/ClassApp');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+    username : String,
+    password : String
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+    username : String,
+    password : String
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+    title : String,
+    description : String,
+    
+    price : Number,
+    Image : String
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
