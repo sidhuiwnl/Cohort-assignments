f80aa6a7c2e70289a0cf75453606b0df986318aa
Sidharth Babu <95430324+sidhuiwnl@users.noreply.github.com>
2024-01-05 23:08:17 +0530

    Card implementation with backened

---
+++ Week-05 Hard Level/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [name,setName] = useState("");
+  const [desc,setDesc] = useState("");
+  const [hobbies,setHobbies] = useState("")
+  
+
+  return (
+    <>
+      <input></input>
+      <input></input>
+    </>
+  )
+}
+
+export default App
